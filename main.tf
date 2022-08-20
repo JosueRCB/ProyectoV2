c7229617664bd211b99c22be530d04ded2d25015
JosueRCB <110694862+JosueRCB@users.noreply.github.com>
2022-08-20 17:02:19 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+
+provider "aws" {
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "proyectov2" {
+  ami           = "ami-01154c8b2e9a14885"
+  instance_type = "t2.micro"
+  security_groups= ["default"]
+  key_name= "proyectoV2California"
+
+  tags = {
+    Name = "proyectoV2"
+  }
+}
+output "instance_public_ip" {
+  description = "Obtener la IP publica de mi instancia"
+  value = aws_instance.proyecto.public_ip  // aws_instance.$NOMBRE-RECURSO-TIPO-aws_instance$.public_ip
+}
